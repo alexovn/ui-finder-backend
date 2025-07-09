c3b8425ce4abd9d3c99ba2f7351276a372ede630
Nikita Alexov <polarbeararmor@gmail.com>
2025-07-09 21:13:27 +0400

    feat: add nginx configuration
    Adds nginx configuration for proxing backend api.
    
    Adds

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.enableCors({
     origin: ['http://localhost:3000'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
 }
