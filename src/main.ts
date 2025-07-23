8706f45d7d20a0b1a7da04680c193b4f1c8e38ef
Nikita Alexov <polarbeararmor@gmail.com>
2025-07-23 23:48:13 +0400

    feat: add domain to CORS whitelist

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://www.ui-finder.dev'],
+    origin: [
+      'http://localhost:3000',
+      'https://ui-finder.dev',
+      'https://www.ui-finder.dev',
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   });
